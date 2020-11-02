895bc8d40b5cccb2c7b1f551dbe5a223baacad37
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-02 19:37:38 +0100

    failing 48_is_not_50_2_but_rather_40_8 test

--- 05/sampleproject/src/roman/test_roman.py
+++ 05/sampleproject/src/roman/test_roman.py
@@ -28,7 +28,7 @@ class RomanNumeralsTest(unittest.TestCase):
 
     def test_20_is_two_x_s(self):
         self.assertEqual(self.temp.roman(27), "XXVII")
-    @unittest.skip
+
     def test_48_is_not_50_2_but_rather_40_8(self):
         self.assertEqual(self.temp.roman(48), "XLVIII")
     @unittest.skip
