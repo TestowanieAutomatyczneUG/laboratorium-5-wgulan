42916372381ca68e32304a881699737a79f1a569
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-02 18:37:09 +0100

    refractor after 3 is three I s test

--- 05/sampleproject/src/roman/roman.py
+++ 05/sampleproject/src/roman/roman.py
@@ -1,8 +1,3 @@
 class Roman:
     def roman(self, arabic):
-        if arabic == 1:
-            return "I"
-        elif arabic == 2:
-            return "II"
-        elif arabic == 3:
-            return "III"
+        return "I" * arabic
