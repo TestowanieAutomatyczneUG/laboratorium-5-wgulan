971f9e895addc4b2fdc1c2fd952f24c16e3e0620
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-02 17:46:36 +0100

    long identical strands test passed

--- 05/sampleproject/src/hamming/test_hamming.py
+++ 05/sampleproject/src/hamming/test_hamming.py
@@ -17,7 +17,7 @@ class HammingTest(unittest.TestCase):
 
     def test_single_letter_different_strands(self):
         self.assertEqual(self.temp.distance("G", "T"), 1)
-    @unittest.skip
+
     def test_long_identical_strands(self):
         self.assertEqual(self.temp.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
     @unittest.skip
