001a28eec85f4129953daaad2621c2af873705f2
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-02 18:06:28 +0100

    passed disallow first strand longer test

--- 05/sampleproject/src/hamming/hamming.py
+++ 05/sampleproject/src/hamming/hamming.py
@@ -3,6 +3,8 @@ class Hamming:
         if gene1 == gene2:
             return 0
         else:
+            if len(gene1) != len(gene2):
+                raise ValueError('Genes have different lenghts')
             diff = 0
             for i in range(0, len(gene1)):
                 if gene1[i] != gene2[i]:
