5977a0278913cbfd76bbceefdf240798368a7be9
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-02 18:01:41 +0100

    passed long different strands test

--- 05/sampleproject/src/hamming/hamming.py
+++ 05/sampleproject/src/hamming/hamming.py
@@ -2,5 +2,9 @@ class Hamming:
     def distance(self, gene1, gene2):
         if gene1 == gene2:
             return 0
-        elif gene1 != gene2:
-            return 1
+        else:
+            diff = 0
+            for i in range(0, len(gene1)):
+                if gene1[i] != gene2[i]:
+                    diff += 1
+            return diff
