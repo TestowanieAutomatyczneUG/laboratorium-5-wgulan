3e2db4c426a51b4fd4e426eab1ee8542bd12924a
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-02 18:08:02 +0100

    refractor after disallow first strand longer test

--- 05/sampleproject/src/hamming/hamming.py
+++ 05/sampleproject/src/hamming/hamming.py
@@ -2,11 +2,10 @@ class Hamming:
     def distance(self, gene1, gene2):
         if gene1 == gene2:
             return 0
-        else:
-            if len(gene1) != len(gene2):
-                raise ValueError('Genes have different lenghts')
-            diff = 0
-            for i in range(0, len(gene1)):
-                if gene1[i] != gene2[i]:
-                    diff += 1
-            return diff
+        elif len(gene1) != len(gene2):
+            raise ValueError('Genes have different lenghts')
+        diff = 0
+        for i in range(0, len(gene1)):
+            if gene1[i] != gene2[i]:
+                diff += 1
+        return diff
