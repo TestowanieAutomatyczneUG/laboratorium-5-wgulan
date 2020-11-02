c3272e7b9d2fa1a87c497d94e89bc16083b90ca9
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-02 18:55:44 +0100

    failing test_20_is_two_x_s test

--- 05/sampleproject/src/roman/test_roman.py
+++ 05/sampleproject/src/roman/test_roman.py
@@ -25,7 +25,7 @@ class RomanNumeralsTest(unittest.TestCase):
 
     def test_9_being_10_1_is_ix(self):
         self.assertEqual(self.temp.roman(9), "IX")
-    @unittest.skip
+
     def test_20_is_two_x_s(self):
         self.assertEqual(self.temp.roman(27), "XXVII")
     @unittest.skip
