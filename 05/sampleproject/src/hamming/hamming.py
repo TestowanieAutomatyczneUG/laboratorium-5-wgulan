c56df0346c7430b6841fb706cf19f884f572ca69
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-02 17:45:20 +0100

    refractor after single letter different strands test

--- 05/sampleproject/src/hamming/hamming.py
+++ 05/sampleproject/src/hamming/hamming.py
@@ -1,8 +1,6 @@
 class Hamming:
     def distance(self, gene1, gene2):
-        if gene1 == "" and gene2 == "":
-            return 0
-        elif gene1 == gene2:
+        if gene1 == gene2:
             return 0
         elif gene1 != gene2:
             return 1
