90a34b2182b0045ac0323fcbdeca9b8199d4210c
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 19:46:30 +0100

    passed verse_one_print test

--- 05/sampleproject/src/song/song.py
+++ 05/sampleproject/src/song/song.py
@@ -1,3 +1,7 @@
 class Song:
     def __init__(self):
-        return
+        self.song = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
+
+    def verse(self, numOfVerse):
+        if numOfVerse == 1:
+            return "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
