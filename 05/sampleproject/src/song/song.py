bb3bcd535a8a166622cdb23204dce2b9e883d431
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 20:18:57 +0100

    passed verse_twelve_print test

--- 05/sampleproject/src/song/song.py
+++ 05/sampleproject/src/song/song.py
@@ -51,4 +51,7 @@ class Song:
             return self.song[9]
         elif numOfVerse == 11:
             return self.song[10]
+        elif numOfVerse == 12:
+            return "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+
 
