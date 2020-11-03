8b95e6535deb7721245ef9c1d539c8c84d91ae99
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 18:50:18 +0100

    failing 400_being_500_100_is_cd test

--- 05/sampleproject/src/roman/test_roman.py
+++ 05/sampleproject/src/roman/test_roman.py
@@ -46,7 +46,7 @@ class RomanNumeralsTest(unittest.TestCase):
 
     def test_60_being_50_10_is_lx(self):
         self.assertEqual(self.temp.roman(163), "CLXIII")
-    @unittest.skip
+
     def test_400_being_500_100_is_cd(self):
         self.assertEqual(self.temp.roman(402), "CDII")
     @unittest.skip
