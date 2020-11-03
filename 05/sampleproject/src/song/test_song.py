289e4a3f6cea0040ddc6feb41f8580a9d5fbf0c3
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 19:48:36 +0100

    fixed tests for Song class

--- 05/sampleproject/src/song/test_song.py
+++ 05/sampleproject/src/song/test_song.py
@@ -10,60 +10,59 @@ class SongTest(unittest.TestCase):
     def test_verse_one_print(self):
         self.assertEqual(self.temp.verse(1),
                          "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.")
-
     @unittest.skip
     def test_verse_two_print(self):
-        self.assertEqual(self.temp.verse(1),
+        self.assertEqual(self.temp.verse(2),
                          "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.")
 
     @unittest.skip
     def test_verse_three_print(self):
-        self.assertEqual(self.temp.verse(1),
+        self.assertEqual(self.temp.verse(3),
                          "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
     @unittest.skip
     def test_verse_four_print(self):
-        self.assertEqual(self.temp.verse(1),
+        self.assertEqual(self.temp.verse(4),
                          "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
     @unittest.skip
     def test_verse_five_print(self):
-        self.assertEqual(self.temp.verse(1),
+        self.assertEqual(self.temp.verse(5),
                          "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
     @unittest.skip
     def test_verse_six_print(self):
-        self.assertEqual(self.temp.verse(1),
+        self.assertEqual(self.temp.verse(6),
                          "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
     @unittest.skip
     def test_verse_seven_print(self):
-        self.assertEqual(self.temp.verse(1),
+        self.assertEqual(self.temp.verse(7),
                          "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
     @unittest.skip
     def test_verse_eight_print(self):
-        self.assertEqual(self.temp.verse(1),
+        self.assertEqual(self.temp.verse(8),
                          "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
     @unittest.skip
     def test_verse_nine_print(self):
-        self.assertEqual(self.temp.verse(1),
+        self.assertEqual(self.temp.verse(9),
                          "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
     @unittest.skip
     def test_verse_ten_print(self):
-        self.assertEqual(self.temp.verse(1),
+        self.assertEqual(self.temp.verse(10),
                          "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
     @unittest.skip
     def test_verse_eleven_print(self):
-        self.assertEqual(self.temp.verse(1),
+        self.assertEqual(self.temp.verse(11),
                          "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
     @unittest.skip
     def test_verse_twelve_print(self):
-        self.assertEqual(self.temp.verse(1),
+        self.assertEqual(self.temp.verse(12),
                          "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
     @unittest.skip
