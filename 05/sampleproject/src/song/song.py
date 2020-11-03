7bbd26bc6e40d8c71a33d904a381d8bec2e51650
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 20:06:28 +0100

    refactor for returning single 1-7 verses

--- 05/sampleproject/src/song/song.py
+++ 05/sampleproject/src/song/song.py
@@ -6,7 +6,9 @@ class Song:
             "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
             "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
             "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-            "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+            "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+
         ]
 
     def verse(self, numOfVerse):
@@ -23,4 +25,4 @@ class Song:
         elif numOfVerse == 6:
             return self.song[5]
         elif numOfVerse == 7:
-            return "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+            return self.song[6]
