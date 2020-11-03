58c21e9323168a0381d7a5ce5f24f43af2ff7853
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 20:02:20 +0100

    refactor for returning single 1-5 verses

--- 05/sampleproject/src/song/song.py
+++ 05/sampleproject/src/song/song.py
@@ -4,7 +4,8 @@ class Song:
             "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
             "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
             "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-            "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+            "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
         ]
 
     def verse(self, numOfVerse):
@@ -17,4 +18,4 @@ class Song:
         elif numOfVerse == 4:
             return self.song[3]
         elif numOfVerse == 5:
-            return "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+            return self.song[4]
