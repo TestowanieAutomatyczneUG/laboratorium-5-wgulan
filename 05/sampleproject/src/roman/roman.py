366bb69a198d5ceec00191611318839c47bd0652
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 18:58:07 +0100

    refactor after 1000_is_a_single_m test

--- 05/sampleproject/src/roman/roman.py
+++ 05/sampleproject/src/roman/roman.py
@@ -1,12 +1,9 @@
 class Roman:
     def roman(self, arabic):
-        rom = ["CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-        arab = [900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        rom = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        arab = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
         res = ''
-
-        if arabic == 1024:
-            return "MXXIV"
 
         for i in range(0, len(arab)):
             while arabic >= arab[i]:
