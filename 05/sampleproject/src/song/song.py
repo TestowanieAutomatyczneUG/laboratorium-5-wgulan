c0b8632008044f1877cac2fe3dfa7ec75185679e
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 19:51:03 +0100

    passed verse_two_print test

--- 05/sampleproject/src/song/song.py
+++ 05/sampleproject/src/song/song.py
@@ -5,3 +5,5 @@ class Song:
     def verse(self, numOfVerse):
         if numOfVerse == 1:
             return "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
+        if numOfVerse == 2:
+            return "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree."
