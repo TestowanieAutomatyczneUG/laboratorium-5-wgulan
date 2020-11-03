f0ed99e8cbb95f12a44bccb8778e1bdbe4c6b3f3
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 19:53:07 +0100

    refactor to return single 1-2 verses

--- 05/sampleproject/src/song/song.py
+++ 05/sampleproject/src/song/song.py
@@ -1,9 +1,12 @@
 class Song:
     def __init__(self):
-        self.song = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
+        self.song = [
+            "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
+            "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree."
+        ]
 
     def verse(self, numOfVerse):
         if numOfVerse == 1:
-            return "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
+            return self.song[0]
         if numOfVerse == 2:
-            return "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree."
+            return self.song[1]
