44a6118518c2fbbb3264fb947cf75e75c09d1539
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 20:00:12 +0100

    passed verse_five_print test

--- 05/sampleproject/src/song/song.py
+++ 05/sampleproject/src/song/song.py
@@ -16,3 +16,5 @@ class Song:
             return self.song[2]
         elif numOfVerse == 4:
             return self.song[3]
+        elif numOfVerse == 5:
+            return "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
