c77fac7cbcc7174380f8c882ffc7916cc4edb740
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 18:53:01 +0100

    refactor after 500_is_a_single_d test

--- 05/sampleproject/src/roman/roman.py
+++ 05/sampleproject/src/roman/roman.py
@@ -1,12 +1,10 @@
 class Roman:
     def roman(self, arabic):
-        rom = ["CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-        arab = [400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        rom = ["D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        arab = [500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
         res = ''
 
-        if arabic == 575:
-            return "DLXXV"
 
         for i in range(0, len(arab)):
             while arabic >= arab[i]:
