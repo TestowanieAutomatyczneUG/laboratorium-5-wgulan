20d65d200da44bdb6c647bca2fa7768828ebf1db
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 18:47:41 +0100

    refactor after 100_is_a_single_c test

--- 05/sampleproject/src/roman/roman.py
+++ 05/sampleproject/src/roman/roman.py
@@ -1,12 +1,9 @@
 class Roman:
     def roman(self, arabic):
-        rom = ["XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-        arab = [90, 50, 40, 10, 9, 5, 4, 1]
+        rom = ["C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        arab = [100, 90, 50, 40, 10, 9, 5, 4, 1]
 
         res = ''
-
-        if arabic == 141:
-            return "CXLI"
 
         for i in range(0, len(arab)):
             while arabic >= arab[i]:
