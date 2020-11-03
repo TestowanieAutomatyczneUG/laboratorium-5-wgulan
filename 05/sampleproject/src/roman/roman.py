6e741512e5622f71a623f341460225d608dcb7df
Wojciech Gulan <wojciech.gulan@gmail.com>
2020-11-03 19:00:11 +0100

    final refactor

--- 05/sampleproject/src/roman/roman.py
+++ 05/sampleproject/src/roman/roman.py
@@ -1,12 +1,13 @@
 class Roman:
-    def roman(self, arabic):
+    def roman(self, number):
         rom = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
         arab = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
         res = ''
 
         for i in range(0, len(arab)):
-            while arabic >= arab[i]:
+            while number >= arab[i]:
                 res += rom[i]
-                arabic -= arab[i]
+                number -= arab[i]
+
         return res
--- 05/sampleproject/src/roman/test_roman.py
+++ 05/sampleproject/src/roman/test_roman.py
@@ -58,7 +58,7 @@ class RomanNumeralsTest(unittest.TestCase):
 
     def test_1000_is_a_single_m(self):
         self.assertEqual(self.temp.roman(1024), "MXXIV")
-        
+
     def test_3000_is_three_m_s(self):
         self.assertEqual(self.temp.roman(3000), "MMM")
     def tearDown(self):
